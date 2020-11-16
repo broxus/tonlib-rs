5e77a7082ddfcd41e9c76a661e98f552a86da09d
Ivan Kalinin <i.kalinin@dexpa.io>
2020-11-16 20:11:28 +0300

    Add async wrapper

--- tonlib-sys/src/lib.rs
+++ tonlib-sys/src/lib.rs
@@ -14,39 +14,64 @@ impl TonlibClient {
         Self(unsafe { trs_create_client() })
     }
 
-    pub fn run<T, F>(&self, function: &T, mut cb: F) -> TonlibResult<()>
+    pub fn run<T, F>(&self, query: &Query<T>, cb: F)
     where
         T: Function,
-        F: FnMut(TonlibResult<T::Reply>) + Send + Sync,
+        F: FnOnce(TonlibResult<T::Reply>) + Send + Sync,
     {
-        let mut buf = Vec::<u8>::new();
-        Serializer::new(&mut buf)
-            .write_boxed(function)
-            .map_err(|e| TonlibError::SerializationError { reason: e.to_string() })?;
-
         let (callback, context) = make_callback(Box::new(move |res: ExecutionResult| cb(ExecutionResultHandle(res).parse())));
-
-        unsafe { trs_run(self.0, buf.as_ptr() as *const c_void, buf.len() as u64, callback, context) };
-
-        Ok(())
+        unsafe { trs_run(self.0, query.0.as_ptr() as *const c_void, query.0.len() as u64, callback, context) };
     }
 
-    pub fn execute<T>(function: &T) -> TonlibResult<T::Reply>
+    pub fn execute<T>(query: &Query<T>) -> TonlibResult<T::Reply>
     where
         T: Function,
     {
-        let mut buf = Vec::<u8>::new();
-        Serializer::new(&mut buf)
-            .write_boxed(function)
-            .map_err(|e| TonlibError::SerializationError { reason: e.to_string() })?;
-
-        ExecutionResultHandle(unsafe { trs_execute(buf.as_ptr() as *const c_void, buf.len() as u64) }).parse()
+        ExecutionResultHandle(unsafe { trs_execute(query.0.as_ptr() as *const c_void, query.0.len() as u64) }).parse()
     }
 }
 
 impl Drop for TonlibClient {
     fn drop(&mut self) {
         unsafe { trs_delete_client(self.0) }
+    }
+}
+
+pub struct Query<T>(Vec<u8>, std::marker::PhantomData<T>);
+
+impl<T> Query<T>
+where
+    T: Function,
+{
+    pub fn new(function: &T) -> TonlibResult<Self> {
+        let mut buf = Vec::<u8>::new();
+        Serializer::new(&mut buf)
+            .write_boxed(function)
+            .map_err(|e| TonlibError::SerializationError { reason: e.to_string() })?;
+
+        Ok(Self(buf, Default::default()))
+    }
+}
+
+pub trait AsQuery {
+    type Function;
+
+    fn as_query(&self) -> TonlibResult<Query<Self::Function>>;
+    fn into_query(self) -> TonlibResult<Query<Self::Function>>;
+}
+
+impl<T> AsQuery for T
+where
+    T: Function,
+{
+    type Function = T;
+
+    fn as_query(&self) -> TonlibResult<Query<Self::Function>> {
+        Query::new(self)
+    }
+
+    fn into_query(self) -> TonlibResult<Query<Self::Function>> {
+        Query::new(&self)
     }
 }
 
@@ -92,12 +117,12 @@ extern "C" {
     fn trs_delete_response(response: *const ExecutionResult);
 }
 
-fn make_callback<A, F: FnMut(A)>(f: Box<F>) -> (unsafe extern "C" fn(*mut c_void, A), *mut c_void)
+fn make_callback<A, F: FnOnce(A)>(f: Box<F>) -> (unsafe extern "C" fn(*mut c_void, A), *mut c_void)
 where
     F: Send + Sync,
 {
     let ptr = Box::into_raw(f);
-    unsafe extern "C" fn callback<A, F: FnMut(A)>(data: *mut c_void, arg: A) {
+    unsafe extern "C" fn callback<A, F: FnOnce(A)>(data: *mut c_void, arg: A) {
         Box::from_raw(data as *mut F)(arg)
     }
     (callback::<A, F>, ptr as *mut c_void)
@@ -107,7 +132,7 @@ where
 mod tests {
     use ton_api::ton;
 
-    use super::TonlibClient;
+    use super::*;
 
     const MAINNET_CONFIG: &str = r#"{
       "liteservers": [
@@ -134,9 +159,13 @@ mod tests {
 
     #[test]
     fn test_static_function() {
-        let result = TonlibClient::execute(&ton::rpc::UnpackAccountAddress {
-            account_address: "-1:3333333333333333333333333333333333333333333333333333333333333333".to_string(),
-        })
+        let result = TonlibClient::execute(
+            &ton::rpc::UnpackAccountAddress {
+                account_address: "-1:3333333333333333333333333333333333333333333333333333333333333333".to_string(),
+            }
+            .as_query()
+            .unwrap(),
+        )
         .unwrap();
 
         println!("{:?}", result);
@@ -144,31 +173,31 @@ mod tests {
 
     #[test]
     fn test_client() {
-        let _ = TonlibClient::execute(&ton::rpc::SetLogVerbosityLevel { new_verbosity_level: 7 }).unwrap();
+        let _ = TonlibClient::execute(&ton::rpc::SetLogVerbosityLevel { new_verbosity_level: 7 }.as_query().unwrap()).unwrap();
 
         let client = TonlibClient::new();
 
         let (tx, rx) = std::sync::mpsc::sync_channel(1);
 
-        client
-            .run(
-                &ton::rpc::Init {
-                    options: ton::options::Options {
-                        config: ton::config::Config {
-                            config: MAINNET_CONFIG.to_string(),
-                            blockchain_name: "mainnet".to_string(),
-                            use_callbacks_for_network: false.into(),
-                            ignore_cache: true.into(),
-                        },
-                        keystore_type: ton::KeyStoreType::KeyStoreTypeInMemory,
+        client.run(
+            &ton::rpc::Init {
+                options: ton::options::Options {
+                    config: ton::config::Config {
+                        config: MAINNET_CONFIG.to_string(),
+                        blockchain_name: "mainnet".to_string(),
+                        use_callbacks_for_network: false.into(),
+                        ignore_cache: true.into(),
                     },
+                    keystore_type: ton::KeyStoreType::KeyStoreTypeInMemory,
                 },
-                move |res| {
-                    println!("Result: {:?}", res);
-                    tx.send(res).unwrap();
-                },
-            )
-            .unwrap();
+            }
+            .as_query()
+            .unwrap(),
+            move |res| {
+                println!("Result: {:?}", res);
+                tx.send(res).unwrap();
+            },
+        );
 
         let _ = rx.recv().unwrap();
     }
--- tonlib/src/lib.rs
+++ tonlib/src/lib.rs
@@ -1,7 +1,143 @@
+use std::future::Future;
+use std::mem::MaybeUninit;
+use std::pin::Pin;
+use std::task::{Context, Poll};
+
+use ton_api::{ton, Function};
+pub use tonlib_sys::errors::*;
+use tonlib_sys::AsQuery;
+
+#[derive(Debug, Clone)]
+pub struct Config {
+    pub network_config: String,
+    pub network_name: String,
+    pub verbosity: u8,
+}
+
+pub struct TonlibClient(tonlib_sys::TonlibClient);
+
+impl TonlibClient {
+    pub async fn new(config: &Config) -> TonlibResult<Self> {
+        let _ = tonlib_sys::TonlibClient::execute(
+            &ton::rpc::SetLogVerbosityLevel {
+                new_verbosity_level: config.verbosity as i32,
+            }
+            .into_query()?,
+        )?;
+
+        let client = TonlibClient(tonlib_sys::TonlibClient::new());
+        client
+            .run(&ton::rpc::Init {
+                options: ton::options::Options {
+                    config: ton::config::Config {
+                        config: config.network_config.to_string(),
+                        blockchain_name: config.network_name.to_string(),
+                        use_callbacks_for_network: false.into(),
+                        ignore_cache: true.into(),
+                    },
+                    keystore_type: ton::KeyStoreType::KeyStoreTypeInMemory,
+                },
+            })
+            .await?;
+
+        Ok(client)
+    }
+
+    async fn run<T>(&self, f: &T) -> TonlibResult<T::Reply>
+    where
+        T: Function,
+    {
+        let mut result = MaybeUninit::uninit();
+        TonlibFuture {
+            client: &self.0,
+            function: Some(f),
+            result: Some(&mut result),
+        }
+        .await;
+        unsafe { result.assume_init() }
+    }
+}
+
+struct TonlibFuture<'f, T>
+where
+    T: Function,
+{
+    client: &'f tonlib_sys::TonlibClient,
+    function: Option<&'f T>,
+    result: Option<&'f mut MaybeUninit<TonlibResult<T::Reply>>>,
+}
+
+impl<'f, T> Future for TonlibFuture<'f, T>
+where
+    T: Function,
+{
+    type Output = ();
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let this = unsafe { self.get_unchecked_mut() };
+
+        match (this.function.take(), this.result.take()) {
+            (Some(f), Some(value)) => {
+                let waker = cx.waker().clone();
+
+                match f.as_query() {
+                    Ok(query) => {
+                        this.client.run::<T, _>(
+                            &query,
+                            Box::new(move |result| {
+                                *value = MaybeUninit::new(result);
+                                waker.wake();
+                            }),
+                        );
+                        Poll::Pending
+                    }
+                    Err(e) => {
+                        *value = MaybeUninit::new(Err(e));
+                        Poll::Ready(())
+                    }
+                }
+            }
+            (None, None) => Poll::Ready(()),
+            _ => unreachable!(),
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    use super::*;
+
+    const MAINNET_CONFIG: &str = r#"{
+      "liteservers": [
+        {
+          "ip": 916349379,
+          "port": 3031,
+          "id": {
+            "@type": "pub.ed25519",
+            "key": "uNRRL+6enQjuiZ/s6Z+vO7yxUUR7uxdfzIy+RxkECrc="
+          }
+        }
+      ],
+      "validator": {
+        "@type": "validator.config.global",
+        "zero_state": {
+          "workchain": -1,
+          "shard": -9223372036854775808,
+          "seqno": 0,
+          "root_hash": "WP/KGheNr/cF3lQhblQzyb0ufYUAcNM004mXhHq56EU=",
+          "file_hash": "0nC4eylStbp9qnCq8KjDYb789NjS25L5ZA1UQwcIOOQ="
+        }
+      }
+    }"#;
+
+    #[tokio::test]
+    async fn test_init() {
+        let _client = TonlibClient::new(&Config {
+            network_config: MAINNET_CONFIG.to_string(),
+            network_name: "mainnet".to_string(),
+            verbosity: 4,
+        })
+        .await
+        .unwrap();
     }
 }
